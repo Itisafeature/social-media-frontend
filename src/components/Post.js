6d8f31b96a0a36108a29c80acbbf531fa1e53e6f
Itisafeature <thebigdebugger@gmail.com>
2020-07-28 23:28:06 -0400

    added update and delete functionality

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import UserContext from '../context/UserContext';
+import { useField } from '../hooks/fields';
 import '../css/Post.css';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleEditPost, handleDeletePost }) => {
+  const [showForm, setShowForm] = useState(false);
+  const content = useField('text');
+  const { user } = useContext(UserContext);
+
   return (
     <div className="post">
       <p className="post__content">{post.content}</p>
       <p className="post__likes">{post.likes}</p>
       <h3 className="post__author">{post.user.username}</h3>
+      {post.user.username === user.username && (
+        <>
+          <button onClick={() => setShowForm(!showForm)}>Edit Post</button>
+          <button onClick={() => handleDeletePost(post)}>Delete Post</button>
+        </>
+      )}
+
+      {showForm && (
+        <form
+          onSubmit={event => handleEditPost(event, post, content, setShowForm)}
+        >
+          <textarea
+            className="content"
+            value={post.content}
+            rows="6"
+            cols="30"
+            {...content}
+          ></textarea>
+          <button type="submit">Update Post</button>
+        </form>
+      )}
     </div>
   );
 };
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -7,6 +7,7 @@ import '../css/Posts.css';
 
 const Posts = ({ history }) => {
   const [posts, setPosts] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
       try {
@@ -23,12 +24,48 @@ const Posts = ({ history }) => {
     setPosts([post].concat(posts));
   };
 
+  const handleEditPost = async (event, post, content, setShowForm) => {
+    event.preventDefault();
+    try {
+      const res = await axios.patch(`/posts/${post._id}`, {
+        content: content.value,
+      });
+      const editedPost = res.data.post;
+      setPosts(
+        posts
+          .map(post => (editedPost._id === post._id ? editedPost : post))
+          .sort((a, b) => new Date(a.updatedAt) > new Date(b.updatedAt))
+      );
+      content.onReset();
+      setShowForm(false);
+    } catch (err) {
+      // TODO: Handle Error
+      console.log(err);
+    }
+  };
+
+  const handleDeletePost = async post => {
+    if (window.confirm('Delete Post?')) {
+      try {
+        await axios.delete(`/posts/${post._id}`);
+        setPosts(posts.filter(el => el._id !== post._id));
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
   return (
     <>
       <PostForm handleNewPost={handleNewPost} />
       <div className="post__container">
         {posts.map(el => (
-          <Post key={el._id} post={el} />
+          <Post
+            key={el._id}
+            post={el}
+            handleDeletePost={handleDeletePost}
+            handleEditPost={handleEditPost}
+          />
         ))}
       </div>
     </>
